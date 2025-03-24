0dee4a3beba3d2b37c6a5688c4f52c47d071560c
Laptop <user@DESKTOP-85SUUJR>
2025-03-24 10:24:15 +0400

    Add project files.

---
+++ lab1_queue/lab1_queue.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Node {
+public:
+    Node(T data, Node* next = nullptr) : _data(data), _nextPtr(next) { }
+    ~Node() {}
+    //private:
+    T _data;
+    Node* _nextPtr;
+};
+
+template <typename T>
+class MyQueue {
+public:
+    MyQueue() : _headPtr(nullptr), _tailPtr(nullptr), _size(0) { }
+
+    ~MyQueue() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    bool isEmpty() { return _size == 0; }
+
+    size_t getSize() { return _size; }
+
+    // Добавление в конец
+    void push(int data) {
+        // Создание новой Node в куче
+        Node<T>* ptrNewNode = new Node<T>(data);
+        if (isEmpty()) {
+            _headPtr = _tailPtr = ptrNewNode;
+        }
+        else {
+            // Хвост смотрит на новую ячейку
+            _tailPtr->_nextPtr = ptrNewNode;
+            // Теперь старый хвост не хвост, и хвостом является новая ячейка
+            // (Обновляем указатель на хвост)
+            _tailPtr = ptrNewNode;
+        }
+        ++_size;
+    }
+
+    // Удаление head
+    void pop() {
+        if (isEmpty()) {
+            throw runtime_error("Can't pop, queue is empty.");
+        }
+        // Запоминаем указатель на голову
+        Node<T>* ptrNodeToDelete = _headPtr;
+
+        // Сдвигаем
+        _headPtr = _headPtr->_nextPtr;
+
+        // Если после этого очередь опустела, обновляем хвост
+        if (_headPtr == nullptr) {
+            _tailPtr = nullptr;
+        }
+
+        delete ptrNodeToDelete;
+        --_size;
+        //return data;
+    }
+
+    // Получение head без удаления
+    int get() {
+        if (isEmpty()) {
+            throw runtime_error("Can't get, queue is empty.");
+        }
+        return _headPtr->_data;
+    }
+
+    // Вывод всей очереди ОТ head ДО tail
+    void printQ() {
+        Node<T>* currPtrNode = _headPtr;
+        while (currPtrNode != nullptr) {
+            cout << currPtrNode->_data;
+
+            currPtrNode = currPtrNode->_nextPtr;
+
+            if (currPtrNode != nullptr) {
+                cout << " --> ";
+            }
+        }
+        cout << '\n';
+    }
+
+    //void DecHead() { --(_headPtr->_data); }
+private:
+    Node<T>* _headPtr;
+    Node<T>* _tailPtr;
+    size_t _size;
+};
+
+//class CashBox {
+//public:
+//    CashBox() : _queue(MyQueue()), _sum(0) { }
+//
+//    ~CashBox() { _queue.~MyQueue(); }
+//
+//    int getSum() { return _sum; }
+//
+//    bool isEmpty() { return _queue.isEmpty(); }
+//
+//    size_t getSize() { return _queue.getSize(); }
+//
+//    void push(int data) { _queue.push(data); _sum += data; }
+//
+//    int pop() {
+//        int temp = _queue.pop();
+//        _sum -= temp;
+//        return temp;
+//    }
+//
+//    int get() { return _queue.get(); }
+//
+//    void printQ() { _queue.printQ(); }
+//
+//    void DecHead() { _queue.DecHead(); --_sum; }
+//private:
+//    MyQueue _queue;
+//    int _sum;
+//};
+
+int main() {
+    string s = "EAS*Y*QUE***ST***IO*N**";
+    string res = "";
+    MyQueue<char> queue;
+
+    for (char ch : s) {
+        if (ch != '*') {
+            queue.push(ch);
+        }
+        else {
+            res += queue.get();
+            queue.pop();
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
---
+++ lab2_stack/lab2_stack.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class NodeInt {
+public:
+    NodeInt(int data, NodeInt* next = nullptr) : _data(data), _nextPtr(next) { }
+    ~NodeInt() {}
+    //private:
+    int _data;
+    NodeInt* _nextPtr;
+};
+
+class Stack {
+public:
+    Stack() : _topPtr(nullptr), _size(0) { }
+
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    bool isEmpty() { return _size == 0; }
+
+    size_t getSize() { return _size; }
+
+    // Добавление
+    void push(int data) {
+        NodeInt* newNodePtr = new NodeInt(data);
+
+        // Новый узел указывает на текущий верхний узел
+        newNodePtr->_nextPtr = _topPtr;
+
+        // Обновляем указатель на вершину
+        _topPtr = newNodePtr;
+
+        ++_size;
+    }
+
+    // Удаление top
+    void pop() {
+        if (isEmpty()) {
+            throw runtime_error("Can't pop, queue is empty.");
+        }
+
+        // Запоминаем указатель на голову
+        NodeInt* nodeToDelete = _topPtr;
+
+        // Сдвигаем
+        _topPtr = _topPtr->_nextPtr;
+
+        delete nodeToDelete;
+        --_size;
+    }
+
+    // Получение top
+    int get() {
+        if (isEmpty()) {
+            throw runtime_error("Can't get, queue is empty.");
+        }
+        return _topPtr->_data;
+    }
+
+    void printS() {
+        NodeInt* currNodePtr = _topPtr;
+
+        cout << "{ ";
+        while (currNodePtr != nullptr) {
+            cout << currNodePtr->_data;
+            currNodePtr = currNodePtr->_nextPtr;
+            if (currNodePtr != nullptr) {
+                cout << " --> ";
+            }
+        }
+        cout << " }";
+    }
+private:
+    NodeInt* _topPtr;
+    size_t _size;
+};
+
+void replaceWithNextGreater(vector<int>& A) {
+    size_t n = A.size();
+    stack <int> st;
+
+    // Проходим массив с конца к началу
+    for (size_t i = A.size(); i > 0; i--) {
+
+        // Удаляем элементы из стека, которые меньше или равны текущему элементу
+        if (st.size() > 0) {
+            while (st.size() > 0 && (st.top() <= A[i - 1])) {
+                st.pop();
+            }
+        }
+
+        // Если стек не пуст, верхний элемент — это ближайший больший элемент
+        // Если стек пуст, заменяем на 0
+        int t = A[i - 1];
+        A[i - 1] = st.size() > 0 ? st.top() : 0;
+
+        // Добавляем текущий элемент в стек
+        st.push(t);
+    }
+    for (size_t i = 0; i < A.size(); i++)
+        cout << A[i] << ' ';
+    cout << '\n';
+}
+
+int main() {
+    vector<int> A = {1, 3, 2, 5, 3, 4};
+
+    replaceWithNextGreater(A);
+
+    for (int num : A) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    Stack stack;
+    vector<int> input_arr = {1, 3, 2, 5, 3, 4};
+
+    for (int el : input_arr) {
+        stack.push(el);
+    }
+
+    stack.printS(); // { 4 --> 3 --> 5 --> 2 --> 3 --> 1 }
+
+    return 0;
+}
